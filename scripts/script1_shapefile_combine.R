17a815c310334ffd9a978090842e617d902225ec
lecrabe <remi.dannunzio@gmail.com>
2017-09-12 00:12:30 +0700

    updated time series

--- scripts/script1_shapefile_combine.R
+++ scripts/script1_shapefile_combine.R
@@ -6,43 +6,56 @@
 rootdir <- "~/khm_ws_20170911/data/"
 setwd(rootdir)
 rootdir <- paste0(getwd(),"/")
-                  
-#################### READ SHAPEFILES
-shp14 <- readOGR(dsn="FC2014_1.shp",layer = "FC2014_1" )
-shp16 <- readOGR(dsn="FC2016.shp",layer = "FC2016" )
 
+#################### SETUP INPUT NAMES
+name_t1 <- "FC2014_1.shp"
+name_t2 <- "FC2016.shp"
+
+attr_t1 <- "FC2014"
+attr_t2 <- "FC2016"
+
+#################### GET BASENAME AND PATH TO FILE
+base_t1 <- basename(name_t1)
+base_t2 <- basename(name_t2)
+
+path_t1 <- dirname(base_t1)
+path_t2 <- dirname(base_t2)
+
+#################### READ SHAPEFILES
+shp_t1 <- readOGR(dsn=paste0(path_t1,"/",base_t1),layer = substr(base_t1,1,nchar(base_t1)-4) )
+shp_t2 <- readOGR(dsn=paste0(path_t2,"/",base_t2),layer = substr(base_t2,1,nchar(base_t2)-4) )
 
 #################### DETERMINE EXTENT OF BOTH SHAPEFILES
-ext <- extent(shp14)
-extent(shp16)
-
+ext <- extent(shp_t1)
 
 #################### EXTRACT DBF AND CHECK DISTRIBUTION OF CLASSES
-dbf14 <- shp14@data
-dbf16 <- shp16@data
+dbf_t1 <- shp_t1@data
+dbf_t2 <- shp_t2@data
 
-names(dbf14)
-names(dbf16)
+write.dbf(dbf_t1,paste0(path_t1,"/bckup_",substr(base_t1,1,nchar(base_t1)-4),".dbf"))
+write.dbf(dbf_t2,paste0(path_t2,"/bckup_",substr(base_t2,1,nchar(base_t2)-4),".dbf"))
 
-table(dbf16$FC2016)
-table(dbf14$FC2014)
+names(dbf_t1)
+names(dbf_t2)
 
+table(dbf_t2[,attr_t2])
+table(dbf_t1[,attr_t1])
 
 #################### GENERATE UNIQUE POLYGON ID AND STANDARDIZE NAMES
-dbf14$polyid <- row(dbf14)[,1]
-dbf16$polyid <- row(dbf16)[,1]
+dbf_t1$polyid <- row(dbf_t1)[,1]
+dbf_t2$polyid <- row(dbf_t2)[,1]
 
-dbf14 <- dbf14[,c("polyid","FC2014")]
-dbf16 <- dbf16[,c("polyid","FC2016")]
+dbf_t1 <- dbf_t1[,c("polyid",attr_t1)]
+dbf_t2 <- dbf_t2[,c("polyid",attr_t2)]
 
-names(dbf14) <- c("polyid_14","class14")
-names(dbf16) <- c("polyid_16","class16")
+names(dbf_t1) <- c("polyid_t1","class_t1")
+names(dbf_t2) <- c("polyid_t2","class_t2")
 
 
 #################### DETERMINE LIST OF CLASSES FOR EACH DATASET
-list_class14 <- levels(as.factor((dbf14$class14)))
-list_class16 <- levels(as.factor((dbf16$class16)))
-list_class   <- unique(list_class16,list_class14)
+list_class_t1 <- levels(as.factor((dbf_t1$class_t1)))
+list_class_t2 <- levels(as.factor((dbf_t2$class_t2)))
+list_class   <- unique(list_class_t2,list_class_t1)
 list_class   <- list_class[order(list_class)]
 
 
@@ -52,54 +65,52 @@ names(code_class) <- c("class","code")
 write.csv(code_class,"code_class.csv",row.names = F)
 
 #################### MERGE THESE CODES IN DBF
-dbf14 <- merge(dbf14,code_class,by.x="class14",by.y="class",all.x=T)
-dbf16 <- merge(dbf16,code_class,by.x="class16",by.y="class",all.x=T)
-
+dbf_t1 <- merge(dbf_t1,code_class,by.x="class_t1",by.y="class",all.x=T)
+dbf_t2 <- merge(dbf_t2,code_class,by.x="class_t2",by.y="class",all.x=T)
 
 #################### EXPORT THE HARMONIZED SHAPEFILES
-shp14@data <- arrange(dbf14,polyid_14)
-shp16@data <- arrange(dbf16,polyid_16)
+out_dbf_t1 <- arrange(dbf_t1,polyid_t1)
+out_dbf_t2 <- arrange(dbf_t2,polyid_t2)
 
-writeOGR(shp14,"shp2014.shp","shp2014",driver="ESRI Shapefile",overwrite_layer = T)
-writeOGR(shp16,"shp2016.shp","shp2016",driver="ESRI Shapefile",overwrite_layer = T)
-
+write.dbf(out_dbf_t1,paste0(path_t1,"/",substr(base_t1,1,nchar(base_t1)-4),".dbf"))
+write.dbf(out_dbf_t2,paste0(path_t2,"/",substr(base_t2,1,nchar(base_t2)-4),".dbf"))
 
 #################### RASTERIZE FIRST SHAPEFILE AT 10m RESOLUTION
 system(sprintf("gdal_rasterize -a %s -l %s -co COMPRESS=LZW -te %s %s %s %s -tr %s %s -ot UInt16 %s %s",
                "code",
-               "shp2014",
+               substr(base_t1,1,nchar(base_t1)-4),
                ext@xmin,ext@ymin,ext@xmax,ext@ymax,
                10,10,
-               "shp2014.shp",
-               "shp2014.tif"
+               base_t1,
+               "shp_t1.tif"
 ))
 
 
 #################### RASTERIZE SECOND SHAPEFILE AT 10m RESOLUTION
 system(sprintf("gdal_rasterize -a %s -l %s -co COMPRESS=LZW -te %s %s %s %s -tr %s %s -ot UInt16 %s %s",
                "code",
-               "shp2016",
+               substr(base_t2,1,nchar(base_t2)-4),
                ext@xmin,ext@ymin,ext@xmax,ext@ymax,
                10,10,
-               "shp2016.shp",
-               "shp2016.tif"
+               base_t2,
+               "shp_t2.tif"
 ))
 
 
 #################### COMBINE BOTH RASTERS INTO A 2_DATES_CODE RASTER
 system(sprintf("gdal_calc.py -A %s -B %s --type=UInt16 --co COMPRESS=LZW --outfile=%s --calc=\"%s\"",
-               "shp2014.tif",
-               "shp2016.tif",
-               "change_1416.tif",
+               "shp_t1.tif",
+               "shp_t2.tif",
+               "change_t1t2.tif",
                "A*100+B"
 ))
 
 
 #################### COMPUTE OCCURENCE OF THE TRANSITION RASTER
 system(sprintf("oft-stat -i %s -o %s -um %s -nostd",
-               "change_1416.tif",
+               "change_t1t2.tif",
                "stats_change.txt",
-               "change_1416.tif"
+               "change_t1t2.tif"
                ))
 
 
@@ -107,24 +118,24 @@ system(sprintf("oft-stat -i %s -o %s -um %s -nostd",
 df <- read.table("stats_change.txt")[,1:2]
 names(df) <- c("chg_code","pix_count")
 
-df$code14 <- as.numeric(substr(as.character(10000 + df$chg_code),2,3))
-df$code16 <- as.numeric(substr(as.character(10000 + df$chg_code),4,5))
+df$code_t1 <- as.numeric(substr(as.character(10000 + df$chg_code),2,3))
+df$code_t2 <- as.numeric(substr(as.character(10000 + df$chg_code),4,5))
 
 
 #################### ORGANIZE AS A TRANSITION MATRIX
-tmp <- data.frame(tapply(df$pix_count*10*10/10000,df[,c("code14","code16")],sum))
-names(tmp) <- c(list_class16)
-tmp$code14 <- c(list_class14)
-tmp[is.na(tmp)]<- 0
+tmp             <- data.frame(tapply(df$pix_count*10*10/10000,df[,c("code_t1","code_t2")],sum))
+names(tmp)      <- c(list_class_t2)
+tmp$code_t1     <- c(list_class_t1)
+tmp[is.na(tmp)] <- 0
 
 #################### WHEN TRANSITIONS ARE NOT OCCURRING, FILL WITH ZEROS
-matrix<-matrix(0,nrow=length(list_class)+1,ncol=length(list_class)+1)
+matrix <- matrix(0,nrow=length(list_class)+1,ncol=length(list_class)+1)
 
 for(i in 1:length(list_class)+1){
   for(j in 1:length(list_class)+1){
     tryCatch({
       print(paste0(i,j))
-    matrix[i,j] <- tmp[tmp$code14 == c("nodata",list_class16)[i] ,c("nodata",list_class16)[j]]
+    matrix[i,j] <- tmp[tmp$code_t1 == c("nodata",list_class_t2)[i] ,c("nodata",list_class_t2)[j]]
     }, error=function(e)cat("Not relevant\n")
     )
   }
@@ -136,13 +147,4 @@ rownames(matrix) <- c("nodata",list_class)
 matrix
 
 #################### EXPORT AS CSV FILE
-write.csv(matrix,"transition_1416.csv")
-
-matrix <- as.matrix(read.csv("transition_1416.csv")[,-1])
-rownames(matrix) <- colnames(matrix)
-
-#################### PLOT MATRIX WITH COLOR GRADIENTS
-matrix <- matrix/sum(matrix)
-matrix <- matrix / max(matrix)
-
-
+write.csv(matrix,"transition_t1_t2.csv")
--- scripts/script2_reclassify.R
+++ scripts/script2_reclassify.R
@@ -11,53 +11,64 @@ rootdir <- paste0(getwd(),"/")
 df <- read.table("stats_change.txt")[,1:2]
 names(df) <- c("chg_code","pix_count")
 
-df$code14 <- as.numeric(substr(as.character(10000 + df$chg_code),2,3))
-df$code16 <- as.numeric(substr(as.character(10000 + df$chg_code),4,5))
+df$code_t1 <- as.numeric(substr(as.character(10000 + df$chg_code),2,3))
+df$code_t2 <- as.numeric(substr(as.character(10000 + df$chg_code),4,5))
 
 #################### READ CODE/CLASS CONVERSION TABLE
-code_class <- read.csv("code_class_agg.csv")
+code_ipcc      <- read.csv("code_ipcc.csv")
+tmp_code_class <- read.csv("code_class.csv")
+code_class     <- merge(tmp_code_class,code_ipcc)
+  
+df1 <- merge(df,code_class[,c("code","class","ipcc_code","ipcc_class")],by.x="code_t1",by.y="code",all.x=T)
+names(df1) <- c("code_t1","chg_code" ,"pix_count","code_t2","class_t1","ipcc_code_t1","ipcc_class_t1")
 
-df1 <- merge(df,code_class[,c("code","class","ipcc_code","ipcc_class")],by.x="code14",by.y="code",all.x=T)
-names(df1) <- c("code14","chg_code" ,"pix_count","code16","class14","ipcc_code_14","ipcc_class_14")
+df1 <- merge(df1,code_class[,c("code","class","ipcc_code","ipcc_class")],by.x="code_t2",by.y="code",all.x=T)
+names(df1) <- c("code_t2","code_t1","chg_code" ,"pix_count","class_t1","ipcc_code_t1","ipcc_class_t1","class_t2","ipcc_code_t2","ipcc_class_t2")
 
-df1 <- merge(df1,code_class[,c("code","class","ipcc_code","ipcc_class")],by.x="code16",by.y="code",all.x=T)
-names(df1) <- c("code16","code14","chg_code" ,"pix_count","class14","ipcc_code_14","ipcc_class_14","class16","ipcc_code_16","ipcc_class_16")
-
-df1$ipcc_chge <- df1$ipcc_code_14 *10 + df1$ipcc_code_16
+df1$ipcc_chge <- df1$ipcc_code_t1 *10 + df1$ipcc_code_t2
 
 df1$final <- 0
 
-table(df1$ipcc_class_14,df1$ipcc_class_16,useNA = "always")
+table(df1$ipcc_class_t1,df1$ipcc_class_t2,useNA = "always")
 
-unique(df1[df1$ipcc_class_16!="F",]$class16)
+unique(df1[df1$ipcc_class_t2!="F",]$class_t2)
 
 nat_forest <- c("B","D","E","Ff","P","Se","M","Mr")
 sec_forest <- c("Tp","Pp","Fr")
-non_forest <- c("Bt","Bu","G","Hc","Hr","R","S","W","Ws","Rp","Po")
+non_forest <- c("Bt","Bu","G","Hr","R","S","W","Ws","Po")
+crop       <- c("Hc")
+rubber     <- c("Rp")
 
 ################## FOREST STABLE
-df1[df1$class14 %in% nat_forest & df1$class16 %in% nat_forest,]$final <- 1
+df1[df1$class_t1 %in% nat_forest & df1$class_t2 %in% nat_forest,]$final <- 1
 
 ################## STABLE NON FOREST
-df1[df1$class14 %in% non_forest & df1$class16 %in% non_forest,]$final <- 2
+df1[df1$class_t1 %in% c(non_forest,crop,rubber) & df1$class_t2 %in% c(non_forest,crop,rubber),]$final <- 2
 
-################## DEFORESTATION
-df1[df1$class14 %in% nat_forest & df1$class16 %in% non_forest,]$final <- 3
-df1[df1$class14 %in% sec_forest & df1$class16 %in% non_forest,]$final <- 3
+################## DEFORESTATION - other
+df1[df1$class_t1 %in% nat_forest & df1$class_t2 %in% non_forest,]$final <- 3
+df1[df1$class_t1 %in% sec_forest & df1$class_t2 %in% non_forest,]$final <- 3
+
+################## DEFORESTATION - crops
+df1[df1$class_t1 %in% nat_forest & df1$class_t2 %in% crop,]$final <- 4
+df1[df1$class_t1 %in% sec_forest & df1$class_t2 %in% crop,]$final <- 4
+
+################## DEFORESTATION - rubber
+df1[df1$class_t1 %in% nat_forest & df1$class_t2 %in% rubber,]$final <- 5
+df1[df1$class_t1 %in% sec_forest & df1$class_t2 %in% rubber,]$final <- 5
 
 ################## FOREST DEGRADATION
-df1[df1$class14 %in% nat_forest & df1$class16 %in% sec_forest,]$final <- 4
+df1[df1$class_t1 %in% nat_forest & df1$class_t2 %in% sec_forest,]$final <- 6
 
 ################## ENHANCEMENT
-df1[df1$class14 %in% sec_forest & df1$class16 %in% nat_forest,]$final <- 5
-df1[df1$class14 %in% non_forest & df1$class16 %in% nat_forest,]$final <- 5
-df1[df1$class14 %in% non_forest & df1$class16 %in% sec_forest,]$final <- 5
+df1[df1$class_t1 %in% sec_forest & df1$class_t2 %in% nat_forest,]$final <- 7
+df1[df1$class_t1 %in% c(non_forest,crop,rubber) & df1$class_t2 %in% nat_forest,]$final <- 7
+df1[df1$class_t1 %in% c(non_forest,crop,rubber) & df1$class_t2 %in% sec_forest,]$final <- 7
 
 ################## MANAGEMENT
-df1[df1$class14 %in% sec_forest & df1$class16 %in% sec_forest,]$final <- 6
+df1[df1$class_t1 %in% sec_forest & df1$class_t2 %in% sec_forest,]$final <- 8
 
-
-df1[df1$final == 0,c("class14","class16")]
+df1[df1$final == 0,c("class_t1","class_t2")]
 table(df1$final)
 
 write.table(df1[,c("chg_code","final")],"reclass.txt",sep = " ",row.names = F,col.names = F)
@@ -67,25 +78,25 @@ write.csv(df1,"all_transitions.csv",row.names = F)
 #################### RECLASSIFY THE CHANGE RASTER
 system(sprintf("(echo %s; echo 1; echo 1; echo 2; echo 0) | oft-reclass -oi  %s  %s",
                "reclass.txt",
-               "tmp_final_chge_1416.tif",
-               "change_1416.tif"
+               "tmp_final_chge_t1_t2.tif",
+               "change_t1t2.tif"
 ))
 
 #################### SIEVE RESULTS
 system(sprintf("gdal_sieve.py -st %s %s %s",
                3,
-               "tmp_final_chge_1416.tif",
-               "tmp_sieve_final_chge_1416.tif"))
+               "tmp_final_chge_t1_t2.tif",
+               "tmp_sieve_final_chge_t1_t2.tif"))
 
 ####################  CREATE A PSEUDO COLOR TABLE
-cols <- col2rgb(c("darkgreen","yellow","red","orange","lightgreen","blue"))
+cols <- col2rgb(c("darkgreen","grey","yellow","pink","red","orange","lightgreen","blue"))
 
-pct <- data.frame(cbind(c(1:6),
+pct <- data.frame(cbind(c(1:8),
                         cols[1,],
                         cols[2,],
                         cols[3,]
 ))
-
+pct
 write.table(pct,paste0("color_table.txt"),row.names = F,col.names = F,quote = F)
 
 
@@ -93,13 +104,13 @@ write.table(pct,paste0("color_table.txt"),row.names = F,col.names = F,quote = F)
 ## Add pseudo color table to result
 system(sprintf("(echo %s) | oft-addpct.py %s %s",
                "color_table.txt",
-               "tmp_sieve_final_chge_1416.tif",
-               "tmp_pct_sieve_final_chge_1416.tif"
+               "tmp_sieve_final_chge_t1_t2.tif",
+               "tmp_pct_sieve_final_chge_t1_t2.tif"
 ))
 
 system(sprintf("gdal_translate -ot byte -co COMPRESS=LZW %s %s",
-               "tmp_pct_sieve_final_chge_1416.tif",
-               "final_change_1416.tif"))
+               "tmp_pct_sieve_final_chge_t1_t2.tif",
+               "final_change_t1_t2.tif"))
 
 #################### DELETE TEMP FILES
 system(sprintf(paste0("rm tmp*.tif")))
